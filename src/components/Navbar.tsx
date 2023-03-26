c27ef73dfbef2afdb5cbf839e27b9f5d2ad7be11
sajjad <samaralishaikh212@gmail.com>
2023-03-26 14:52:15 +0530

    add: toggle theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 import SignInButton from "@/ui/SignInButton";
 import SignOutButton from "@/ui/SignOutButton";
 import { buttonVariants } from "@/ui/Button";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const Navbar = async () => {
   const session = await getServerSession();
@@ -12,11 +13,13 @@ const Navbar = async () => {
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-stone-900 z-50 top-0 left-0 right-0 h-20 border-b border-stone-300 dark:border-stone-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
         <Link href="/" className={buttonVariants({ variant: "link" })}>
-          Text Analaysis 1.0
+          Text Analaysis 1.0 👑🔮
         </Link>
-        <div className="md:hidden">{/* <ThemeToggle /> */}</div>
+        <div className="md:hidden">
+          <ThemeToggle />
+        </div>
         <div className="hidden md:flex gap-4">
-          {/* <ThemeToggle /> */}
+          <ThemeToggle />
           <Link
             href="/documentation"
             className={buttonVariants({ variant: "ghost" })}
