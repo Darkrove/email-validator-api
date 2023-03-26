02b86b52c6212729d529170ef70b5b4a40f59859
sajjad <samaralishaikh212@gmail.com>
2023-03-26 17:51:15 +0530

    update: logo name 🎪

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-stone-900 z-50 top-0 left-0 right-0 h-20 border-b border-stone-300 dark:border-stone-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
         <Link href="/" className={buttonVariants({ variant: "link" })}>
-          Text Analaysis 1.0 👑🔮
+          Text Analaysis API 1.0
         </Link>
         <div className="md:hidden">
           <ThemeToggle />
