00ef9e950d96420b17ec2cbb3f39ce642ef33765
sajjad <samaralishaikh212@gmail.com>
2023-04-30 13:11:31 +0530

    add API with middleware: create key

---
+++ src/pages/api/api-key/create.ts
@@ -0,0 +1,62 @@
+import { withMethods } from "@/lib/api-middleware/with-methods";
+import { authOptions } from "@/lib/auth";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { CreateApiData } from "@/types/api";
+import db from "@/lib/prisma";
+import { nanoid } from "nanoid";
+import { z } from "zod";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<CreateApiData>
+) => {
+  try {
+    const user = await getServerSession(req, res, authOptions).then(
+      (res) => res?.user
+    );
+
+    if (!user) {
+      return res.status(401).json({
+        error: "Unauthorized",
+        createdApiKey: null,
+      });
+    }
+    const existingApiKey = await db.apiKey.findFirst({
+      where: {
+        userId: user.id,
+        enabled: true,
+      },
+    });
+
+    if (existingApiKey) {
+      return res.status(400).json({
+        error: "You already have an API key",
+        createdApiKey: null,
+      });
+    }
+
+    const createdApiKey = await db.apiKey.create({
+      data: {
+        userId: user.id,
+        key: nanoid(),
+      },
+    });
+    return res.status(200).json({
+      error: null,
+      createdApiKey: createdApiKey,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        error: error.issues,
+        createdApiKey: null,
+      });
+    }
+    return res.status(500).json({
+      error: "Internal Server Error",
+      createdApiKey: null,
+    });
+  }
+};
+export default withMethods(["GET"], handler);
