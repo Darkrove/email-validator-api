53ef7c2f13113126976ec8936b239a3427ed288c
sajjad <samaralishaikh212@gmail.com>
2023-05-02 13:15:15 +0530

    update: separator color

--- src/components/ui/DropdownMenu.tsx
+++ src/components/ui/DropdownMenu.tsx
@@ -162,7 +162,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-stone-200 dark:bg-stone-900", className)}
+    className={cn("-mx-1 my-1 h-px bg-stone-200 dark:bg-stone-600", className)}
     {...props}
   />
 ));
